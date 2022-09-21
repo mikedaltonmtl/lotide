57d1ec5be06cd4e12622ce9eb1ebf3f033e9ac6b
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-09-21 16:22:35 +0000

    initial assertion file added

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😀😀😀 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😡😡😡 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 5);
+assertEqual("1", 1);
+assertEqual(false, null);
