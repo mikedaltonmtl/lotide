3a581765a92426912bb1c60b610554133798dc92
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-09-23 13:55:39 +0000

    refactor head.js

--- head.js
+++ head.js
@@ -1,27 +1,18 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1, 5);
-// assertEqual("1", 1);
-// assertEqual(false, null);
-
 // function to return the first item of a given array
-const head = function(testArray) {
-  return testArray[0];
+const head = function(array) {
+  return array[0];
 };
 
 // test code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+assertEqual(head([5,6,7]), 5); // PASS
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // PASS
+assertEqual(head([5]), 5);  // PASS
+assertEqual(head([]), 5);   // FAIL
