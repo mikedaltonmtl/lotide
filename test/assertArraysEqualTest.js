2cd08925827eb79ed3a38c04fac6b7c6fa894a21
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-10 17:59:24 +0000

    refactor middle & add middleTest

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,11 +1,11 @@
 // Import the assertArraysEqual module.
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([], []); // should pass
+assertArraysEqual([], []); // should PASS
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]);  // should fail
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]);  // should FAIL
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // should pass
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // should fail
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // should FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // should FAIL
