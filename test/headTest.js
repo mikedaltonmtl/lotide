f7cbb46b8ed0163a58a4dcbf90bb766d560f01a6
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-10 18:27:26 +0000

    added chai and refactored headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,27 @@
-// Import the assertEqual module.
-const assertEqual = require('../assertEqual');
-
 // Import the head module.
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5); // PASS
-assertEqual(head([5,6,7]), 6); // FAIL
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // PASS
-assertEqual(head([5]), 5);  // PASS
-assertEqual(head([]), 5);   // FAIL
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns 0 for [0]", () => {
+    assert.strictEqual(head([0]), 0);
+  });
+
+});
